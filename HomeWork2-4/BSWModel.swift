ccc449e7be038ee89f09be82310b03ba6f473175
moremist <rbest15@rambler.ru>
2020-04-08 16:29:25 +0300

    Initial Commit

---
+++ HomeWork2-4/BSWModel.swift
@@ -0,0 +1,8 @@
+import Foundation
+import ReactiveKit
+import Bond
+import Alamofire
+
+class BTWModel {
+    var categoryArray = MutableObservableArray<String>()
+}
---
+++ HomeWork2-4/BSWTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BSWTableViewCell.swift
+//  HomeWork2-4
+//
+//  Created by Roman Kuzmich on 08.04.2020.
+//  Copyright © 2020 Roman Kuzmich Home Dev. All rights reserved.
+//
+
+import UIKit
+
+class BSWTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
